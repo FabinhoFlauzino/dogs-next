c5fcb30b081e95a92a7ab5bde9716168e23826ab
Fabinho Flauzino <FabioFlauzino@hotmail.com>
2025-08-12 22:35:28 -0300

    Feed conta

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { PHOTOS_GET } from "@/functions/api";
+import apiError from "@/functions/api-error";
+
 export type Photo = {
   id: number;
   author: string;
@@ -12,11 +15,25 @@ export type Photo = {
   total_comments: string;
 }
 
-export default async function photosGet() {
-  const response = await fetch('https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0', {
-    next: {revalidate: 10, tags: ['photos']}
-  })
-  const data = await response.json() as Photo[]
-  
-  return data
+type PhotosGetParams = {
+  page?: number;
+  total?: number;
+  user?: 0 | string;
+}
+
+export default async function photosGet({ page = 1, total = 6, user = 0 }: PhotosGetParams = {}) {
+  try {
+    const { url } = PHOTOS_GET({ page, total, user });
+    const response = await fetch(url, {
+      next: { revalidate: 10, tags: ['photos'] }
+    })
+
+    if (!response.ok) throw new Error("Erro ao carregar fotos.")
+
+    const data = await response.json() as Photo[]
+    return { data, ok: true }
+  } catch (error: unknown) {
+    return apiError(error)
+  }
+
 }
--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,12 +1,30 @@
+import photosGet from "@/actions/photos-get"
+import userGet from "@/actions/user-get"
+import Feed from "@/components/feed/feed"
 import { Metadata } from "next"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "Minha Conta",
-
 }
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const {data: user} = await userGet()
+  const { data } = await photosGet({user: user?.username})
+
   return (
-    <div></div>
+    <main>
+      {data?.length ? (
+        <Feed  photos={data}/>
+      ) : (
+        <div className="container">
+          <h1 className="title">Não há fotos</h1>
+          <p className="text">Você ainda não possui fotos, que tal começar a compartilhar?</p>
+          <Link href="/conta/postar" className="button" style={{display: 'inline-block'}}>
+            Postar foto
+          </Link>
+        </div>
+      )}
+    </main>
   )
 }
